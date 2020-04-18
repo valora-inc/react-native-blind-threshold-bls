085099967f24d76b1e2bd820eed81b529fafaa49
J M Rossy <jm.rossy@gmail.com>
2020-04-17 22:55:51 -0400

    Get communication with libs working using JNA

--- README.md
+++ README.md
@@ -5,8 +5,19 @@
 `$ npm install react-native-blind-threshold-bls --save`
 
 ## Usage
+
 ```javascript
 import BlindThresholdBls from 'react-native-blind-threshold-bls';
 
 BlindThresholdBls;
+```
+
+## Proguard
+
+To accomodate JNA, add the following to the consuming app's proguard rules:
+
+```
+-dontwarn java.awt.*
+-keep class com.sun.jna.* { *; }
+-keepclassmembers class * extends com.sun.jna.* { public *; }
 ```
--- android/src/main/java/org/celo/BlindThresholdBlsModule.java
+++ android/src/main/java/org/celo/BlindThresholdBlsModule.java
@@ -5,12 +5,29 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+
+import android.util.Log;
+
 public class BlindThresholdBlsModule extends ReactContextBaseJavaModule {
 
     static {
-        System.loadLibrary("blind_threshold_bls");
+        Native.register(BlindThresholdBlsModule.class, "blind_threshold_bls");
     }
 
+// TODO cleanup
+//    class BlindedMessage {
+//        public Buffer message;
+//        public String blinding_factor;
+//
+//        BlindedMessage(Buffer message, String blinding_factor) {
+//            this.message = message;
+//            this.blinding_factor = blinding_factor;
+//        }
+//    }
+
+    private static final String TAG = "BlindThresholdBlsModule";
     private final ReactApplicationContext reactContext;
 
     public BlindThresholdBlsModule(ReactApplicationContext reactContext) {
@@ -24,8 +41,22 @@ public class BlindThresholdBlsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void blindMessage(String message, Callback callback) {
+        try {
+            Log.d(TAG, "Blinding message" );
+            byte[] buffer = new byte[512];
+            IntByReference bufferSize = new IntByReference(buffer.length);
+            blind(message, "my awesome seed must be at least 32 characters long", buffer, bufferSize);
+            Log.d(TAG, "Received buffer size: " + bufferSize.getValue() );
+            String blinded = new String(buffer, 0, bufferSize.getValue());
+            Log.d(TAG, "Received blinded message: " + blinded );
+            callback.invoke(blinded);
+        } catch (Exception e) {
+            Log.e(TAG, "Exception while blinding the message: " + e.getMessage() );
+        }
+
     }
+
+    // Seed must be >= 32 characters long
+    private static native void blind(String message, String seed, byte[] blinded_message, IntByReference blinded_message_size);
 }
