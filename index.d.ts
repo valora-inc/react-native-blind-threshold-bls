8c530199d6b9dbf695c8baef60eabe0773b40d19
J M Rossy <jm.rossy@gmail.com>
2020-04-22 17:48:57 -0400

    Fix type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,6 @@
-function blindMessage(message: string): Promise<string>;
-
-function unblindMessage(base64BlindedSignature: string): Promise<string>;
-
-const BlindThresholdBls = {
-  blindMessage,
-  unblindMessage,
-};
+declare namespace BlindThresholdBls {
+  function blindMessage(message: string): Promise<string>;
+  function unblindMessage(base64BlindedSignature: string): Promise<string>;
+}
 
 export default BlindThresholdBls;
