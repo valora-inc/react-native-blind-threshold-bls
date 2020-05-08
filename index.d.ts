a06682da3dca79b04614feb81faf91e62961a33d
J M Rossy <jm.rossy@gmail.com>
2020-05-08 18:37:30 -0400

    Fix OOM error on iOS and align text encoding btwn iOS and Android (#2)

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare namespace BlindThresholdBls {
   function blindMessage(message: string): Promise<string>;
   function unblindMessage(
     base64BlindedSignature: string,
-    signerPublicKey: string
+    base64SignerPublicKey: string
   ): Promise<string>;
 }
 
--- ios/BlindThresholdBls.m
+++ ios/BlindThresholdBls.m
@@ -3,7 +3,6 @@
 #import <React/RCTLog.h>
 #import <Foundation/Foundation.h>
 #import <stdlib.h>
-#import <stdint.h>
 
 @implementation BlindThresholdBls {
   Token_PrivateKey* blindingFactor;
@@ -12,7 +11,7 @@
 
 RCT_EXPORT_MODULE()
 
-// TODO add support for multilpe outstanding blind calls
+// TODO add support for multiple outstanding blind calls
 RCT_REMAP_METHOD(blindMessage,
                  message:(NSString *) message
                  resolver:(RCTPromiseResolveBlock)resolve
@@ -20,34 +19,37 @@ RCT_REMAP_METHOD(blindMessage,
 {
   @try {
     RCTLogInfo(@"Preparing blind message buffers");
-    NSData *messageData = [message dataUsingEncoding:NSUTF8StringEncoding];
-    messageBuf.ptr = (uint8_t*)[messageData bytes];
+    NSData *messageData = [[NSData alloc] initWithBase64EncodedString:message options:0];
+    messageBuf.ptr = [BlindThresholdBls nsDataToByteArray:messageData];
     messageBuf.len = [messageData length];
-//    uint8_t * messageBytes = malloc(length);
-//    memcpy(messageBytes, [messageData bytes], length);
+
     Buffer blindedMessageBuf;
+    blindedMessageBuf.ptr = NULL;
+    blindedMessageBuf.len = 0;
     
     RCTLogInfo(@"Preparing blinding seed");
-    NSMutableData* seedData = [NSMutableData dataWithCapacity:32];
-    int status = SecRandomCopyBytes(kSecRandomDefault, 32, seedData.mutableBytes);
+    uint8_t *seedData = malloc(sizeof(uint8_t) * 32);
+    int status = SecRandomCopyBytes(kSecRandomDefault, 32, seedData);
     if (status != errSecSuccess) {
       [NSException raise:@"Random bytes copy failed" format:@"status code %d", status];
     }
     Buffer seedBuf;
-    seedBuf.ptr = (uint8_t*)[seedData bytes];
+    seedBuf.ptr = seedData;
     seedBuf.len = 32;
 
     RCTLogInfo(@"Calling blind");
     blind(&messageBuf, &seedBuf, &blindedMessageBuf, &blindingFactor);
 
     RCTLogInfo(@"Blind call done, retrieving blinded message from buffer");
-    int blindedMessageLen = blindedMessageBuf.len;
+    const size_t blindedMessageLen = blindedMessageBuf.len;
+    const uint8_t* blindedMessagePtr = blindedMessageBuf.ptr;
     NSMutableData* blindedMessageData = [NSMutableData dataWithCapacity:blindedMessageLen];
-    [blindedMessageData appendBytes:blindedMessageBuf.ptr length:blindedMessageLen];
+    [blindedMessageData appendBytes:blindedMessagePtr length:blindedMessageLen];
     NSString *blindedMessageBase64 = [blindedMessageData base64EncodedStringWithOptions:0];
     
     RCTLogInfo(@"Cleaning Up Memory");
-    free_vector(blindedMessageBuf.ptr, blindedMessageBuf.len);
+    free_vector(blindedMessagePtr, blindedMessageLen);
+    free(seedBuf.ptr);
     
     resolve(blindedMessageBase64);
   }
@@ -60,7 +62,7 @@ RCT_REMAP_METHOD(blindMessage,
 
 RCT_REMAP_METHOD(unblindMessage,
                  base64BlindedSignature:(NSString *) base64BlindedSignature
-                 signerPublicKey:(NSString *) signerPublicKey
+                 base64SignerPublicKey:(NSString *) base64SignerPublicKey
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
@@ -68,49 +70,69 @@ RCT_REMAP_METHOD(unblindMessage,
     RCTLogInfo(@"Preparing unblind buffers");
     NSData *blindedSigData = [[NSData alloc] initWithBase64EncodedString:base64BlindedSignature options:0];
     Buffer blindedSigBuf;
-    blindedSigBuf.ptr = (uint8_t*)[blindedSigData bytes];
+    blindedSigBuf.ptr = [BlindThresholdBls nsDataToByteArray:blindedSigData];
     blindedSigBuf.len = [blindedSigData length];
+
     Buffer unblindedSigBuf;
+    unblindedSigBuf.ptr = NULL;
+    unblindedSigBuf.len = 0;
     
     RCTLogInfo(@"Calling unblind");
     unblind(&blindedSigBuf, blindingFactor, &unblindedSigBuf);
 
     RCTLogInfo(@"Unblind call done, deserializing public key");
-    NSData *publicKeyData = [signerPublicKey dataUsingEncoding:NSUTF8StringEncoding];
-    uint8_t* publicKeyBytes = (uint8_t*)[publicKeyData bytes];
-    PublicKey* publicKey;
+    NSData *publicKeyData = [[NSData alloc] initWithBase64EncodedString:signerPublicKey options:0];
+    uint8_t* publicKeyBytes = [BlindThresholdBls nsDataToByteArray:publicKeyData];
+    PublicKey* publicKey = NULL;
     deserialize_pubkey(publicKeyBytes, &publicKey);
 
     RCTLogInfo(@"Verifying the signatures");
-    BOOL signatureValid = NO;
+    bool signatureValid = false;
+    // Verify may throw if the signatures are not correct
     @try {
-      // Verify throws if the signatures are not correct
-      verify(publicKey, &messageBuf, &unblindedSigBuf);
-      signatureValid = YES;
+      signatureValid = verify(publicKey, &messageBuf, &unblindedSigBuf);
     }
     @catch (NSException *exception) {
-      reject(@"Key verification error", @"Invalid threshold signature", nil);
+      signatureValid = false;
+      RCTLogInfo(@"Invalid threshold signature found when verifying");
     }
 
-    if (signatureValid == YES) {
+    if (signatureValid == true) {
       RCTLogInfo(@"Verify call done, retrieving signed message from buffer");
-      int unblindedSigLen = unblindedSigBuf.len;
+      const size_t unblindedSigLen = unblindedSigBuf.len;
       NSMutableData* unblindedSigData = [NSMutableData dataWithCapacity:unblindedSigLen];
       [unblindedSigData appendBytes:unblindedSigBuf.ptr length:unblindedSigLen];
       NSString *b64UnblindedSig = [unblindedSigData base64EncodedStringWithOptions:0];
       resolve(b64UnblindedSig);
     }
+    else {
+      reject(@"Key verification error", @"Invalid threshold signature", nil);
+    }
 
     RCTLogInfo(@"Cleaning Up Memory");
+    free_vector(unblindedSigBuf.ptr, unblindedSigBuf.len);
     destroy_token(blindingFactor);
     blindingFactor = NULL;
-    free_vector(unblindedSigBuf.ptr, unblindedSigBuf.len);
     destroy_pubkey(publicKey);
+    free(publicKeyBytes);
+    free(blindedSigBuf.ptr);
+    free(messageBuf.ptr);
+    messageBuf.ptr = NULL;
+    messageBuf.len = 0;
+   
   } 
   @catch (NSException *exception) {
     RCTLogInfo(@"Exception while unblinding the signature: %@", exception.reason); 
     reject(@"Unblinding error", exception.reason, nil);
   }
+}
+
++ (uint8_t*) nsDataToByteArray: (NSData*)data
+{
+  NSUInteger len = [data length];
+  uint8_t *byteData = malloc(sizeof(uint8_t) * len);
+  [data getBytes:byteData length:len];
+  return byteData;
 }
 
 // TODO implement a cleanup method that destroys token if user cancels btwn blinding and unblinding
