d03cb4692a50e6383b27785f4ad7202cfcbd8f7f
codyborn <codyborn@outlook.com>
2021-09-22 15:45:29 -0700

    Remove createJSModules override

--- android/src/main/java/org/celo/BlindThresholdBlsPackage.java
+++ android/src/main/java/org/celo/BlindThresholdBlsPackage.java
@@ -20,9 +20,4 @@ public class BlindThresholdBlsPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-    
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 }
